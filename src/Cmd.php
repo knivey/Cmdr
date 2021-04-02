15a33b20a84716a9a36ce7244b2d18a21742e51c
Fred Akers <knivey@botops.net>
2021-04-02 08:10:32 -0400

    add call

--- src/Cmd.php
+++ src/Cmd.php
@@ -7,11 +7,13 @@ class Cmd
     public string $command;
     public string $syntax;
     public string $method;
+    public array $callargs;
 
-    public function __construct(string $command, string $method, string $syntax)
+    public function __construct(string $command, string $method, array $callargs, string $syntax)
     {
         $this->command = $command;
         $this->method = $method;
         $this->syntax = $syntax;
+        $this->callargs = $callargs;
     }
 }
--- src/Cmdr.php
+++ src/Cmdr.php
@@ -12,11 +12,11 @@ class Cmdr
         $this->cmds = new CIArray();
     }
 
-    function add(string $command, callable $method, string $syntax = '') {
+    function add(string $command, callable $method, array $callargs, string $syntax = '') {
         if (str_contains($command, '#')) {
             throw new \Exception('Command name cannot contain #');
         }
-        $this->cmds[$command] = new Cmd($command, $method, $syntax);
+        $this->cmds[$command] = new Cmd($command, $method, $callargs, $syntax);
     }
 
     function get(string $command, string $text) : Request {
@@ -24,6 +24,11 @@ class Cmdr
 	    $args = new Args($cmd->syntax);
 	    $args->parse($text);
 	    return new Request($args, $cmd);
+    }
+
+    function call(string $command, string $text) {
+	    $req = $this->get($command, $text);
+	    call_user_func_array($req->cmd->method, [$req->cmd->callargs, $req]);
     }
 
 }
