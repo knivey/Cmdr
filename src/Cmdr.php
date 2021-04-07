e851d55a3b6a15721515b96952cac6b1d3d9a03a
Fred Akers <knivey@botops.net>
2021-04-07 03:25:34 -0400

    Dont call commands that dont exist

--- src/Cmdr.php
+++ src/Cmdr.php
@@ -19,7 +19,10 @@ class Cmdr
         $this->cmds[$command] = new Cmd($command, $method, $callArgs, $syntax);
     }
 
-    function get(string $command, string $text) : Request {
+    function get(string $command, string $text) : Request|false {
+	    if(!isset($this->cmds[$command])) {
+	        return false;
+        }
 	    $cmd = $this->cmds[$command];
 	    $args = new Args($cmd->syntax);
 	    $args->parse($text);
@@ -28,7 +31,9 @@ class Cmdr
 
     function call(string $command, string $text, array $extraArgs) {
 	    $req = $this->get($command, $text);
-	    call_user_func_array($req->cmd->method, [...$req->cmd->callArgs, ...$extraArgs, $req]);
+	    if(!$req)
+	        return false;
+	    return call_user_func_array($req->cmd->method, [...$req->cmd->callArgs, ...$extraArgs, $req]);
     }
 
 }
